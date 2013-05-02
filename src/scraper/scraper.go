644dcbee95f8bad68c7c079f910a150a9aa58858
Roger Jungemann <roger@thefifthcircuit.com>
2013-05-01 23:06:10 -0700

    refactored

--- src/scraper/scraper.go
+++ src/scraper/scraper.go
@@ -27,40 +27,8 @@ func main() {
 	api := anaconda.NewTwitterApi("19683-Wd51DLxYGmQmNqGxOVKQqs7w31Ho2GfazxUkwCHds", "vSfKzTlQz3HZXSO0bMjg4yXVYgUodMyc7l378USeM8")
 
 	var max_id int64 = -1
-	var last_max_id int64 = -1
-	var since_id int64 = -1
-
-	v := url.Values{}
-	v.Set("screen_name", "rjungemann")
-	v.Set("count", "200")
-
-	if since_id != -1 {
-		v.Set("since_id", fmt.Sprintf("%v", since_id))
-	}
-
-	if max_id != -1 {
-		v.Set("max_id", fmt.Sprintf("%v", max_id))
-	}
-
-	searchResult, err := api.GetUserTimeline(v)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, tweet := range searchResult {
-		user := tweet.User
-		user_id := user.Id
-		created_at := tweet.Created_at
-		remote_id := tweet.Id
-		text := tweet.Text
-
-		_, err = stmtIns.Exec(user_id, created_at, remote_id, text)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		max_id = tweet.Id
-	}
+	var last_max_id int64 = 0
+	var since_id int64 = -1 // TODO: handle since_id
 
 	for {
 		if last_max_id == max_id {
@@ -85,8 +53,6 @@ func main() {
 		}
 
 		last_max_id = max_id
-
-		fmt.Println(len(searchResult))
 
 		for _, tweet := range searchResult {
 			user := tweet.User
